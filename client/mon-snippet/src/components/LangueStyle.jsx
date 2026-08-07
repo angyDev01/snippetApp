ed71d130dfa608f1e3bca61d0dbadde61bf59bf7
Angy <yaojeanangoa@gmail.com>
2026-08-07 02:58:39 +0000

    Amélioration de la copie sur ios

--- client/mon-snippet/src/components/LangueStyle.jsx
+++ client/mon-snippet/src/components/LangueStyle.jsx
@@ -3,15 +3,16 @@
 // variable pour styler chaque tag selon la valeur qu'il contient
 
 const COULEURS_LANGAGE = {
-    python: { backgroundColor: "#7CCAD5", color: "#f0ffff" },
+    python: { backgroundColor: "#e4d617", color: "#f0ffff" },
     css: { backgroundColor: "#E85C90", color: "#ffffff" },
-    javascript: { backgroundColor: "#eab308", color: "#000000" }
+    javascript: { backgroundColor: "#f7a409", color: "#0e0d0d" },
+    html: { backgroundColor: "#472aee", color: "#dfd7d7" }
 };
 
 // Hook appliquant cette logique
 export default function LangueStyle({ langage }) {
     const cleLangage = langage.toLowerCase();
-    const styleApplique = COULEURS_LANGAGE[cleLangage] || { backgroundColor: "#6b7280", color: "#ffffff" };
+    const styleApplique = COULEURS_LANGAGE[cleLangage] || { backgroundColor: "#ec9614", color: "#ffffff" };
 
     const styleDeBase = {
         padding: "5px 5px",
